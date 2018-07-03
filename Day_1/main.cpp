0c050c70db5fd6ec59519d8875c34b89f7a6106c
Kirill Krasilnikov <kirill.a.krasilnikov@gmail.com>
2018-07-03 08:45:33 +0300

    added main.cpp, CMakeLists.txt  and video
    Signed-off-by: Kirill Krasilnikov <kirill.a.krasilnikov@gmail.com>

---
+++ Day_1/main.cpp
@@ -0,0 +1,26 @@
+#include "/opt/opencv/modules/highgui/include/opencv2/highgui/highgui.hpp"
+#include "/opt/opencv/include/opencv2/opencv.hpp"
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+	VideoCapture cap("Dogfight.mp4"); // open the video file for reading
+	if ( !cap.isOpened() )
+	return -1;
+	//cap.set(CV_CAP_PROP_POS_MSEC, 300);
+	double fps = cap.get(CV_CAP_PROP_FPS);
+	//start the video at 300ms
+	//get the frames per seconds of the video
+	std::cout << "Frame per seconds : " << fps << std::endl;
+	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+	while(1) {
+		Mat frame;
+		bool bSuccess = cap.read(frame); // read a new frame from video
+		if (!bSuccess) {
+			std::cout << "Cannot read the frame from video file" << std::endl;
+			break;
+		}
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+		if(waitKey(30) == 27) {
+		break;
+	}
+}
